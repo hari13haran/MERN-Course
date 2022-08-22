af06c63b28c63b38987c4a0eea5fca61c3f41d61
hari13haran <h.hariran@gmail.com>
2022-08-22 23:53:10 +0530

    Workout Controller - Pt.1

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,48 @@
+const Workout = require('../models/workoutModel.js')
+const mongoose = require('mongoose')
+
+// Get all workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+}
+
+// Get a single workout
+const getWorkout = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({err: 'No such workout found!'})
+    }
+    
+    const workout = await Workout.findById(id)
+    if(!workout) {
+        return res.status(404).json({err: 'No such workout found!'})
+    }
+    res.status(200).json(workout)
+}
+
+// Create a new workout
+const createWorkout =  async (req, res) => {
+    
+    // request body payload
+    const {title, load, reps} = req.body
+    
+    // add data to DB
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (err) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+// Delete a workout
+
+// Update a workout
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+}
