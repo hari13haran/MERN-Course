14443e95a502b5e15c9edd406f2e04d4a77c3f3e
hari13haran <h.hariran@gmail.com>
2022-08-30 22:15:09 +0530

    React context implementation

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm';
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const Home = () => {
     
-    const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch } = useWorkoutsContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
             const response = await fetch('/api/workouts')
             const jsonData = await response.json()
             if(response.ok) {
-                setWorkouts(jsonData)
+                dispatch({type: 'SET_WORKOUTS', payload: jsonData})
             }
         }
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
 
     return ( 
         <div className="home">
