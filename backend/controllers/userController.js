92d2790bccaabded79fb21e6f1cb6f5c7ec59bc2
hari13haran <h.hariran@gmail.com>
2022-09-22 22:52:07 +0530

    create JWT : signUp method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// create JWT function
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 // login user function
 const loginUser = async (req,res) => {
@@ -10,7 +16,9 @@ const signupUser = async (req,res) => {
     const {email, password} = req.body
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+        // create a token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
