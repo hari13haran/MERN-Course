e2ff27c24108d09fc5cef81d599dbef1c1db1567
hari13haran <h.hariran@gmail.com>
2022-10-02 23:32:08 +0530

    Making Authorized GET & POST req from React

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm';
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Home = () => {
     
     const { workouts, dispatch } = useWorkoutsContext();
+    const { user } = useAuthContext();
+    const [isAuthorized, setIsAuthorized] = useState(null);
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const jsonData = await response.json()
             if(response.ok) {
                 dispatch({type: 'SET_WORKOUTS', payload: jsonData})
             }
         }
-        fetchWorkouts()
-    }, [dispatch])
+        if (user) {
+            fetchWorkouts()
+            setIsAuthorized(true)
+        }
+    }, [dispatch, user])
 
     return ( 
         <div className="home">
@@ -24,8 +34,9 @@ const Home = () => {
                 {workouts && workouts.map((workout) => (
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
+                { !isAuthorized && <span>Please Login / Sign Up</span> }
             </div>
-            <WorkoutForm />
+            { isAuthorized && <WorkoutForm /> }
         </div>
      );
 }
