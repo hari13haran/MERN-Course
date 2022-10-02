d4ba8fe99338e01c87aecd346da2bca7dfcdf87d
hari13haran <h.hariran@gmail.com>
2022-10-03 00:42:23 +0530

    Added user_id to workouts & conditions to fetch

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,14 +1,17 @@
 import { useAuthContext } from "./useAuthContext"
+import { useWorkoutsContext } from "./useWorkoutsContext"
 
 export const useLogout = () => {
 
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch } = useWorkoutsContext()
     
     const logout = () => {
         // remove the local storage
         localStorage.removeItem('user')
         // update Auth context - Global storage
         dispatch({type: 'LOGOUT'})
+        workoutsDispatch({type: 'SET_WORKOUTS', payload: null})
     }
 
     return { logout }
